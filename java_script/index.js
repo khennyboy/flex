348040f40aec2e2cd9ae795801e93fd53c7f0415
Sheriff baba <abdullateefkehinde848@gmail.com>
2023-09-08 19:44:17 +0100

    done

---
+++ java_script/index.js
@@ -0,0 +1,9 @@
+cars = ["bentley", "volvo", "bughatti", "benz"];
+
+console.log(cars.length);
+console.log(cars[1]);
+console.log(cars);
+
+for (i = 0; i < cars.length; i++) {
+  console.log("I have a " + cars[i]);
+}
